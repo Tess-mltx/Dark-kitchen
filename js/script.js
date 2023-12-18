78bfca34d3e4f067c15f8d38b3d9e2d15d966e56
Tess <tess@DESKTOP-9F1581K>
2023-12-18 11:33:59 +0100

    structure js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+let sectionCard = document.getElementById("burgersSection");
+function GenerateCard(burgers) {
+    let article = document.createElement("article");
+    let image = document.createElement("img");
+    let contentSection = document.createElement("section");
+    let title = document.createElement("h3");
+    let ulCategories = document.createElement("ul");
+    let liCategorie;
+    let containerIcon = document.createElement("span");
+    let icon = document.createElement("i");
+    let description = document.createElement("p");
+    let price = document.createElement('strong');
+    let quantity = document.createElement("input");
+    let addPanier = document.createElement("button");
 
+    
+}
 
 toggleDarkMode.addEventListener('change', (e) =>{ // <--- modifier pour charger une autre stylesheet ???
     if (e.target.value = 'dark') {
